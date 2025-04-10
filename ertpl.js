d0a7af2dca3ed0a2bc0b99a6f96b29def310a903
Vichingo455 <administrator@vichingo455.duckdns.org>
2025-04-10 14:53:30 +0200

    Finalizza la parte urbana START

--- ertpl.js
+++ ertpl.js
@@ -1,8 +1,15 @@
 /*
 The function below is used to fall back to the second (not for importance) server if the first is not available
 Swaps both links and images sources
+aid = identifier for the anchor (a tag)
+imgid = identifier for the image (img tag)
+path = path for the image starting from the root, for example /Dani/10225.jpg
 */
 function changeUrlToFallback(aid,imgid,path) {
-    document.getElementById(aid).href = "http://serverissimo.freeddns.org:30081/apps/files_sharing/publicpreview/ffdqobqRg2ezKXt?file=" + path + "&x=1920&y=1080&a=true";
-    document.getElementById(imgid).src = "http://serverissimo.freeddns.org:30081/apps/files_sharing/publicpreview/ffdqobqRg2ezKXt?file=" + path + "&x=1920&y=1080&a=true";
+    try {
+        document.getElementById(imgid).src = "http://serverissimo.freeddns.org:30081/apps/files_sharing/publicpreview/ffdqobqRg2ezKXt?file=" + path + "&x=1920&y=1080&a=true";
+    } catch {}
+    try {
+        document.getElementById(aid).href = "http://serverissimo.freeddns.org:30081/apps/files_sharing/publicpreview/ffdqobqRg2ezKXt?file=" + path + "&x=1920&y=1080&a=true";
+    } catch {}
 }
