ee6fd0b28398c5304dfd36efcad3b8c9a31559ce
Vichingo455 <vichingo455@gmail.com>
2025-05-20 12:20:21 +0200

    Fix vari e TPER work in progress

---
+++ scripts/tpersearch.js
@@ -0,0 +1,52 @@
+const searchBar = document.getElementById('searchBar');
+const productsContainer = document.getElementById('bus-container');
+const buttons = document.getElementById('buttons');
+
+let allProducts = [];
+window.onbeforeunload=searchBar.value="";
+
+const url = '/scripts/tperbus.json';
+fetch(url)
+    .then(response => {
+        if (!response.ok) throw new Error("Errore nel caricamento dei dati.");
+        return response.json();
+    })
+    .then(data => {
+        allProducts = data;
+    })
+    .catch(error => console.error('Errore nel caricamento dei dati:', error));
+
+searchBar.addEventListener('input', () => {
+    if (searchBar.value == '') {
+        productsContainer.innerHTML = ' ';
+        buttons.innerHTML = `
+        <div class="verticale">
+            <a href="/tper_menu/tpersub.html" class="button">Suburbano</a>
+            <a href="/tper_menu/tperextra.html" class="button">Extraurbano</a>
+        </div>
+        `;
+        return;
+    }
+    buttons.innerHTML = ' ';
+        const searchTerm = searchBar.value.toLowerCase();
+        const filtered = allProducts.filter(bus =>
+            bus.matricola.toLowerCase().includes(searchTerm)
+        );
+        renderProducts(filtered);
+});
+
+function renderProducts(products) {
+    productsContainer.innerHTML = '';
+    products.forEach(bus => {
+        const div = document.createElement('div');
+        div.className = 'product-card';
+        div.innerHTML = `
+            <a href="${bus.link}">
+                <h3>${bus.matricola}</h3>
+                <p>${bus.modello}</p>
+                <p>${bus.settore}</p>
+            </a>
+        `;
+        productsContainer.appendChild(div);
+    });
+}
