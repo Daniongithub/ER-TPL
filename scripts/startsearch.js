2f99dfd5329aa2edcb319373422ee7f6e4152493
Curry141 <135717077+Daniongithub@users.noreply.github.com>
2025-06-23 10:44:19 +0200

    New stuff

--- scripts/startsearch.js
+++ scripts/startsearch.js
@@ -26,9 +26,13 @@ searchBar.addEventListener('input', () => {
             <a href="/start_menu/startextra.html" class="button">Extraurbano</a>
         </div>
         <div style="height: 35px;"></div>
-        <div>
+        <div class="verticale">
             <a class="gbutton" href="/startravenna_bus/listamezzi.html">Lista mezzi</a>
+            <a class="gbutton" href="/start_menu/servizi/start-fermatebus/">Visualizzatore fermate</a>
+            <a class="gbutton" href="/start_menu/servizi/start-livebus/">Autobus in tempo reale</a>
+            <a class="gbutton" href="/start_menu/servizi/start-corsesopp/">Corse non garantite</a>
         </div>
+        <p>I servizi "Visualizzatore fermate", "Autobus in tempo reale" e "Corse non garantite" fanno uso di dati forniti da servizi Start Romagna. <br>L'unica cosa che facciamo è ottenerli e impaginarli in maniera differente dai servizi Start, spesso perchè fatti male.</p>
         `;
         return;
     }
