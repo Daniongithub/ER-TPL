811ed04e236fcbde03668ce83484c05e4d0405b9
Leocraft1 <leonardotoschi07@gmail.com>
2025-07-26 18:52:06 +0200

    Aggiunto input manuale codice fermate cercaorario seta

--- seta_menu/cercaorario/js/cercafermata.js
+++ seta_menu/cercaorario/js/cercafermata.js
@@ -1,8 +1,10 @@
 const searchBar = document.getElementById('searchBar');
+const stopCodeBar = document.getElementById('stopCodeBar');
 const resultsContainer = document.getElementById('searchResults');
 
 let allresults = [];
 window.onbeforeunload=searchBar.value="";
+window.onbeforeunload=stopCodeBar.value="";
 
 const url = 'https://setaapi.serverissimo.freeddns.org/stoplist';
 fetch(url)
@@ -16,14 +18,66 @@ fetch(url)
     .catch(error => console.error('Errore nel caricamento dei dati:', error));
 
 searchBar.addEventListener('input', () => {
-    const footer=document.getElementById('footer');
-    footer.innerHTML='';
     const searchTerm = searchBar.value.toLowerCase();
     const filtered = allresults.filter(item =>
         item.fermata.toLowerCase().includes(searchTerm)
     );
     renderresults(filtered);
 });
+
+stopCodeBar.addEventListener('input', () => {
+    var code=stopCodeBar.value.toUpperCase();
+    code="MO"+code;
+    //renderresultscode(filtered);
+    const searchResultsContainer = document.getElementById('searchResults');
+    const warning=document.getElementById('warning-mo');
+    warning.innerHTML='';
+    searchResultsContainer.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.className = 'search-result';
+    div.innerHTML = `
+        <div>
+            <h3>${code}</h3>
+            <p>Codice fermata: ${code}</p>
+        </div>
+    `;
+
+    div.addEventListener('click', () => {
+        const url = `fermata.html?code=${encodeURIComponent(code)}&name=${encodeURIComponent(code)}`;
+        parent.location=url;
+    });
+
+    searchResultsContainer.appendChild(div);
+    if (stopCodeBar.value == '') {
+        resultsContainer.innerHTML='';
+        warning.innerHTML = `
+        Attenzione! Se la palina non riporta MO all'inizio del codice, il MO viene inserito in automatico!
+        `;
+        return;
+    }
+});
+
+function renderresultscode(results) {
+    const searchResultsContainer = document.getElementById('searchResults');
+    searchResultsContainer.innerHTML = '';
+
+    const div = document.createElement('div');
+    div.className = 'search-result';
+    div.innerHTML = `
+        <div>
+            <h3>${results.fermata}</h3>
+            <p>Codice fermata: ${results.valore}</p>
+        </div>
+    `;
+
+    div.addEventListener('click', () => {
+        const url = `fermata.html?code=${encodeURIComponent(item.valore)}&name=${encodeURIComponent(item.fermata)}`;
+        parent.location=url;
+    });
+
+    searchResultsContainer.appendChild(div);
+}
 
 function renderresults(results) {
     const searchResultsContainer = document.getElementById('searchResults');
@@ -46,7 +100,7 @@ function renderresults(results) {
 
         div.addEventListener('click', () => {
             const url = `fermata.html?code=${encodeURIComponent(item.valore)}&name=${encodeURIComponent(item.fermata)}`;
-            window.open(url, '_blank');
+            parent.location=url;
         });
 
         searchResultsContainer.appendChild(div);
