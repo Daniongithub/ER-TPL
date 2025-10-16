4910db509f5328e45af4a5b94de65b39346e3c68
Leocraft1 <leonardotoschi07@gmail.com>
2025-10-16 13:55:06 +0200

    Aggiornamento frequenze

--- seta_menu/frequenze/js/infos.js
+++ seta_menu/frequenze/js/infos.js
@@ -1,6 +1,8 @@
 const params = new URLSearchParams(window.location.search);
 const num = params.get('routenum');
 const lineaSpan = document.getElementById('linea-span');
+const descContainer = document.getElementById('desc-container');
+const linkContainer = document.getElementById('link-a-seta');
 
 const urlDesc = "https://setaapi.serverissimo.freeddns.org/frequencydescription/"+num;
 
@@ -9,3 +11,36 @@ lineaSpan.innerHTML=num;
 
 //Contenuto del testo
 fetch(urlDesc)
+    .then(response => {
+        if (!response.ok) throw new Error("Impossibile raggiungere il server.");
+        return response.json();
+    })
+    .then(data =>{
+        descContainer.innerHTML=`
+        <table id="desc-table">
+            <th>Destinazione</th>
+            <th>Tempo di percorrenza</th>
+            <th>Frequenza</th>
+        </table>`;
+        const table = document.getElementById('desc-table');
+        if(data[0].linea==undefined){
+            descContainer.innerHTML="La linea specificata non possiede nessuna descrizione valida.";
+        }
+        var i=0;
+        data[0].destinazioni.forEach(element => {
+            var tr = document.createElement('tr');
+            var td = document.createElement('td');
+            td.innerHTML=data[0].destinazioni[i];
+            tr.appendChild(td);
+            var td = document.createElement('td');
+            td.innerHTML=data[0].percorrenza[i];
+            tr.appendChild(td);
+            var td = document.createElement('td');
+            td.innerHTML=data[0].frequenze[i];
+            tr.appendChild(td);
+            table.appendChild(tr);
+            i++;
+        });
+        linkContainer.innerHTML=`<a href="${data[0].linkseta}" class="biancosott">Visualizza sul sito seta</a>`;
+    })
+    .catch(error => {descContainer.innerHTML="La linea specificata non possiede nessuna descrizione valida.";console.error(error);})
