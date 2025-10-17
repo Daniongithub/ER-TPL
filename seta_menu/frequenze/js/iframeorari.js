dba381fa8ec762a461eaf7f56557bca6b03f5c02
Leocraft1 <leonardotoschi07@gmail.com>
2025-10-17 17:18:34 +0200

    Prova

--- seta_menu/frequenze/js/iframeorari.js
+++ seta_menu/frequenze/js/iframeorari.js
@@ -1,46 +1,25 @@
 const params = new URLSearchParams(window.location.search);
 const num = params.get('routenum');
 const lineaSpan = document.getElementById('linea-span');
-const iframeContainer = document.getElementById('iframe-container');
+const iframe = document.getElementById('iframe');
 const linkContainer = document.getElementById('link-a-seta');
 
-var setaURL = "https://setaapi.serverissimo.freeddns.org/frequencydescription/"+num;
+const date = new Date();
+const todayDate=date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
+
+const baseURL = "https://setaapi.serverissimo.freeddns.org/lineedyn_linea_dett_percorsi/?b=mo&l=MO"+num+"&dd="+todayDate+"&v=As";
 
 //Display numero linea
 lineaSpan.innerHTML=num;
 
-//Contenuto del testo
-fetch(urlDesc)
-    .then(response => {
-        if (!response.ok) throw new Error("Impossibile raggiungere il server.");
-        return response.json();
-    })
-    .then(data =>{
-        descContainer.innerHTML=`
-        <table id="desc-table">
-            <th>Destinazione</th>
-            <th>Tempo di percorrenza</th>
-            <th>Frequenza</th>
-        </table>`;
-        const table = document.getElementById('desc-table');
-        if(data[0].linea==undefined){
-            descContainer.innerHTML="La linea specificata non possiede nessuna descrizione valida.";
-        }
-        var i=0;
-        data[0].destinazioni.forEach(element => {
-            var tr = document.createElement('tr');
-            var td = document.createElement('td');
-            td.innerHTML=data[0].destinazioni[i];
-            tr.appendChild(td);
-            var td = document.createElement('td');
-            td.innerHTML=data[0].percorrenza[i];
-            tr.appendChild(td);
-            var td = document.createElement('td');
-            td.innerHTML=data[0].frequenze[i];
-            tr.appendChild(td);
-            table.appendChild(tr);
-            i++;
-        });
-        linkContainer.innerHTML=`<a href="${data[0].linkseta}" class="biancosott">Visualizza sul sito seta</a>`;
-    })
-    .catch(error => {descContainer.innerHTML="La linea specificata non possiede nessuna descrizione valida.";console.error(error);})
+//Spawn iframe
+iframe.setAttribute("src",baseURL);
+setTimeout(function(){}, 2000);
+
+//Adatta altezza iframe da codice injectato
+window.addEventListener("message", (event) => {
+    if (event.origin !== "https://setaapi.serverissimo.freeddns.org/") return; // sicurezza
+    if (event.data?.type === "resize") {
+        frame.style.height = event.data.height + "px";
+    }
+});
