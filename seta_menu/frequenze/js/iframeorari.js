282e1a29e9fbc5479ae5e043baa9d2dec9acd95e
Leocraft1 <leonardotoschi07@gmail.com>
2025-10-17 17:27:49 +0200

    Tempfix

--- seta_menu/frequenze/js/iframeorari.js
+++ seta_menu/frequenze/js/iframeorari.js
@@ -14,12 +14,3 @@ lineaSpan.innerHTML=num;
 
 //Spawn iframe
 iframe.setAttribute("src",baseURL);
-setTimeout(function(){}, 2000);
-
-//Adatta altezza iframe da codice injectato
-window.addEventListener("message", (event) => {
-    if (event.origin !== "https://setaapi.serverissimo.freeddns.org/lineedyn_linea_dett_percorsi") return; // sicurezza
-    if (event.data?.type === "resize") {
-        frame.style.height = event.data.height + "px";
-    }
-});
