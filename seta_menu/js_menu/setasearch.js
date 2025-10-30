41da0539e4a25ba20fce9182a9f5fad7cccf2966
Leocraft1 <leonardotoschi07@gmail.com>
2025-10-30 13:53:42 +0100

    Mosso setasearch + display none bottoni + fix grafica ricerca + novità

--- seta_menu/js_menu/setasearch.js
+++ seta_menu/js_menu/setasearch.js
@@ -5,7 +5,7 @@ const buttons = document.getElementById('buttons');
 let allProducts = [];
 window.onbeforeunload=searchBar.value="";
 
-const url = '/scripts/setabus.json';
+const url = '/seta_menu/js_menu/setabus.json';
 fetch(url)
     .then(response => {
         if (!response.ok) throw new Error("Errore nel caricamento dei dati.");
@@ -18,35 +18,11 @@ fetch(url)
 
 searchBar.addEventListener('input', () => {
     if (searchBar.value == '') {
-        productsContainer.innerHTML = ' ';
-        buttons.innerHTML = `
-        <div>
-            <a href="/seta_menu/setaurbano.html" class="button">Urbano</a>
-            <a href="/seta_menu/setasub.html" class="button">Suburbano</a>
-            <a href="/seta_menu/setaextra.html" class="button">Extraurbano</a>
-            <a href="/seta_menu/setafilobus.html" class="button">Filobus</a>
-            <a href="/setamodena_bus/minibus/sprinter.html" class="button">Minibus</a>
-            <div style="height: 8px;"></div>
-            <a href="/setamodena_bus/servizio.html" class="button">Veicoli di servizio</a>
-            <div style="height: 8px;"></div>
-            <a href="/seta_menu/radiatimenu.html" class="rbutton">Veicoli radiati</a>
-        </div>
-        <div style="height: 35px;"></div>
-        <div>
-            <a class="gbutton" href="atcmlalinea.html">Archivio La Linea ATCM</a>
-            <a class="gbutton" href="setastreetview.html">Storico StreetView</a>
-        </div>
-        <div style="height: 5px;"></div>
-        <h2>Servizi SETA Modena:</h2>
-        <div>
-            <a class="gbutton" href="cercaorario/index.html">Orari in tempo reale</a>
-            <a class="gbutton" href="businservizio/index.html">Monitor autobus in servizio</a>
-            <a class="gbutton" href="percorsi/index.html">Percorsi</a>
-        </div>
-        `;
+        productsContainer.innerHTML = '';
+        buttons.style.display="";
         return;
     }
-    buttons.innerHTML = ' ';
+    buttons.style.display="none";
     const searchTerm = searchBar.value.toLowerCase();
     const filtered = allProducts.filter(bus => bus.matricola.toLowerCase().includes(searchTerm))
     .sort((a, b) => {
