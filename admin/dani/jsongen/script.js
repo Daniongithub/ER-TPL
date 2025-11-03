94d9b7dc8e79d9a813e130e7ba01aedc4fd22d3e
Curry141 <135717077+Daniongithub@users.noreply.github.com>
2025-11-03 18:49:43 +0100

    Updating my swiss-knife tool for jsons.

--- admin/dani/jsongen/script.js
+++ admin/dani/jsongen/script.js
@@ -1,29 +1,34 @@
 function genera() {
-const matrda = document.getElementById("matrda").value.trim();
-const matra = document.getElementById("matra").value.trim();
-const selezione = document.getElementById("selezione").value.trim();
-const modello = document.getElementById("modello").value.trim();
-const link = document.getElementById("link").value.trim();
-const startMatricola = parseInt(matrda);
-const endMatricola = parseInt(matra);
-let jsonArray = [];
-for (let i = startMatricola; i <= endMatricola; i++) {
-	link2 = link + "#" + i.toString();
-    let baseJson = {
-        "matricola": i.toString(),
-        "settore": selezione,
-        "modello": modello,
-        "link": link2
-    };
+    const matrda = document.getElementById("matrda").value.trim();
+    const matra = document.getElementById("matra").value.trim();
+    const selezione = document.getElementById("selezione").value.trim();
+    const modello = document.getElementById("modello").value.trim();
+    const link = document.getElementById("link").value.trim();
+    const flag = document.getElementById("flag");
+    const startMatricola = parseInt(matrda);
+    const endMatricola = parseInt(matra);
+    let jsonArray = [];
+    for (let i = startMatricola; i <= endMatricola; i++) {
+        let link2 = link + "#" + i.toString();
+        let tipo = "settore";
+        if (flag.checked){
+            tipo = "vettore"
+        }
+        let baseJson = {
+            "matricola": i.toString(),
+            [tipo]: selezione,
+            "modello": modello,
+            "link": link2
+        };
+        jsonArray.push(baseJson);
+    }
+    document.getElementById("output").textContent =
+        jsonArray.map((item, index) => {
+            const jsonStr = JSON.stringify(item, null, 2);
+            return (index < jsonArray.length - 1) ? jsonStr + "," : jsonStr;
+        }).join("\n");
+}
 
-    jsonArray.push(baseJson);
-}
-document.getElementById("output").textContent = 
-    jsonArray.map((item, index) => {
-        const jsonStr = JSON.stringify(item, null, 2);
-        return (index < jsonArray.length - 1) ? jsonStr + "," : jsonStr;
-    }).join("\n");
-}
 function copiaOutput() {
     const code = document.getElementById("output").innerText;
     navigator.clipboard.writeText(code).then(() => {
@@ -31,6 +36,7 @@ function copiaOutput() {
         alert("Errore durante la copia.");
     });
 }
+
 function pulisci() {
     document.getElementById("matrda").value = "";
     document.getElementById("matra").value = "";
