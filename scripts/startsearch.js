245db5c13f3996a86faf6aed40a1a46f9d60af1e
Curry141 <135717077+Daniongithub@users.noreply.github.com>
2025-12-21 15:23:01 +0100

    New photos + renaming + chores.

--- scripts/startsearch.js
+++ scripts/startsearch.js
@@ -21,19 +21,24 @@ searchBar.addEventListener('input', () => {
         productsContainer.innerHTML = ' ';
         buttons.innerHTML = `
         <div>
-            <a href="/start_menu/starturbano.html" class="blue button">Urbano</a>
-            <a href="/start_menu/startsub.html" class="blue button">Suburbano</a>
-            <a href="/start_menu/startextra.html" class="blue button">Extraurbano</a>
-        </div>
-        <hr class="solid">
-        <h2>Servizi START Romagna</h2>
-        <div>
-            <a class="green button" href="/start_menu/servizi/start-livebus/">Autobus in tempo reale</a>
-            <a class="green button" href="/start_menu/servizi/start-corsesopp/">Corse non garantite</a>
-            <a class="green button" href="/startravenna_bus/listamezzi.html">Lista mezzi</a>
-            <a class="green button" href="/start_menu/servizi/start-fermatebus/">Visualizzatore fermate</a>
-        </div>
-        <p>I servizi "Autobus in tempo reale", "Corse non garantite" e "Visualizzatore fermate" fanno uso di dati forniti dai servizi di Start Romagna. <br>L'unica cosa che facciamo è ottenerli e impaginarli in maniera differente dai siti di Start per esigenze nostre.</p>
+      <a href="/start_menu/starturbano.html" class="blue button">Urbano</a>
+      <a href="/start_menu/startsub.html" class="blue button">Suburbano</a>
+      <a href="/start_menu/startextra.html" class="blue button">Extraurbano</a>
+    </div>
+    <div>
+      <h3>Ambito di Ravenna</h3>
+      <a href="/ravenna_menu/mete.html" class="blue button">Privati METE S.p.A.</a>
+      <a href="/ravenna_menu/articoli.html" class="green button">Articoli</a>
+    </div>
+    <hr class="solid">
+    <h2>Servizi START Romagna</h2>
+    <div>
+      <a class="green button" href="/start_menu/servizi/start-livebus/">Autobus in tempo reale</a>
+      <a class="green button" href="/start_menu/servizi/start-corsesopp/">Corse non garantite</a>
+      <a class="green button" href="/start_bus/listamezzi.html">Lista mezzi</a>
+      <a class="green button" href="/start_menu/servizi/start-fermatebus/">Visualizzatore fermate</a>
+    </div>
+    <p>I servizi "Autobus in tempo reale", "Corse non garantite" e "Visualizzatore fermate" fanno uso di dati forniti dai servizi di Start Romagna. <br>L'unica cosa che facciamo è ottenerli e impaginarli in maniera differente dai siti di Start per esigenze nostre.</p>
         `;
         return;
     }
