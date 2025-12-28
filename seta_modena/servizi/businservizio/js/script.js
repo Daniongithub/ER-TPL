8c9c749d5e1a2776471356b33bd8a6d0b22aa33c
Leocraft1 <leonardotoschi07@gmail.com>
2025-12-28 18:13:15 +0100

    Soft maintenance

--- seta_modena/servizi/businservizio/js/script.js
+++ seta_modena/servizi/businservizio/js/script.js
@@ -44,96 +44,13 @@ fetch(urlModels)
                 model!="Irisbus Cityclass CNG ex Pavia"&&
                 model!="Mercedes Integro O550 (Giallo)"
             ){
-                option.value = modelsDictionary(model);
+                option.value = model;
                 option.textContent = model;
                 modelloSelect.appendChild(option);
             }
         });
     })
     .catch(error => {console.error('Errore nel caricamento dei dati:', error)});
-
-
-function modelsDictionary(model){
-    return model;
-    //Non serve piu! :D
-    /*
-    if(model=="Irisbus Citelis CNG EEV"){
-        return "Irisbus Citelis CNG";
-    }
-    if(model=="Irisbus Crossway"){
-        return "Irisbus Crossway";
-    }
-    if(model=="Irisbus Crossway ex Esercito Tedesco"){
-        return "Irisbus Crossway Esercito";
-    }
-    if(model=="Iveco Urbanway Mild Hybrid 2022"){
-        return "Iveco Urbanway Mild Hybrid CNG";
-    }
-    if(model=="MenariniBus Citymood LNG"){
-        return "Menarinibus Citymood LNG";
-    }
-    if(model=="MenariniBus Citymood CNG"){
-        return "Menarinibus Citymood CNG";
-    }
-    if(model=="Iveco Crossway LE 12 CNG"){
-        return "Iveco Crossway LE CNG";
-    }
-    if(model=="Iveco Crossway LE"){
-        return "Iveco Crossway LE Diesel";
-    }
-    if(model=="Iveco Crossway Line"){
-        return "Iveco Crossway Line";
-    }
-    if(model=="Mercedes Integro O550"||model=="Mercedes Integro O550 (Giallo)"){
-        return "Mercedes Integro";
-    }
-    if(model=="Scania Irizar i4 LNG"){
-        return "Irizar i4 LNG";
-    }
-    if(model=="Solaris Urbino 12 III CNG"){
-        return "Solaris Urbino 12 CNG";
-    }
-    if(model=="Iveco Crossway Line 12 CNG"){
-        return "Iveco Crossway Line CNG";
-    }
-    if(model=="MAN Lion's Regio"){
-        return "MAN Lion's Regio";
-    }
-    if(model=="Setra S415 LE 2p ex Bolzano"){
-        return "Setra ex Bolzano (2 porte)";
-    }
-    if(model=="Setra S415 LE 3p ex Bolzano"){
-        return "Setra ex Bolzano (3 porte)";
-    }
-    if(model=="Iveco Crossway LE 14"){
-        return "Iveco Crossway LE 14m";
-    }
-    if(model=="MAN Lion's City 19 CNG"){
-        return "New MAN Lion's City 19G";
-    }
-    if(model=="Solaris Trollino 12 IV"){
-        return "Solaris Trollino 12";
-    }
-    if(model=="Irisbus Cityclass CNG ATCM"){
-        return "Irisbus Cityclass CNG ATCM";
-    }
-    if(model=="Mercedes Citaro O530N CNG"){
-        return model;
-    }
-    if(model=="Iveco Crossway LE 14"){
-        return "Iveco Crossway LE 14m";
-    }
-    if(model=="Iveco Crossway LE 14"){
-        return "Iveco Crossway LE 14m";
-    }
-    if(model=="Iveco Crossway LE 14"){
-        return "Iveco Crossway LE 14m";
-    }
-    if(model=="Iveco Crossway LE 14"){
-        return "Iveco Crossway LE 14m";
-    }
-    */
-}
 
 caricadati();
 var refreshGeneraleID=setInterval(caricadati, 60000);
@@ -178,9 +95,14 @@ function caricadati(){
             }else{
                 var posizione=element.next_stop;
             }
+            if(element.br==true){
+                var dest = element.destination1+"<br>"+destination2;
+            }else{
+                var dest = element.route_desc;
+            }
             tr.innerHTML = `
                 <td>${element.linea}</td>
-                <td>${element.route_desc}</td>
+                <td>${dest}</td>
                 <td><a href="infoveicolo.html?id=${element.vehicle_code}" class="bianco">${element.vehicle_code}</a></td>
                 <td>${element.model}</td>
                 <td>${posizione}</td>
