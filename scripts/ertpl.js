ff466cb34f7763dfd83f51e01283387eb100030d
Leocraft1 <leonardotoschi07@gmail.com>
2025-12-29 16:05:26 +0100

    No true for SETA

--- scripts/ertpl.js
+++ scripts/ertpl.js
@@ -1,28 +1,3 @@
-/*
-The function(s) below are used to fall back to the second (not for importance) server if the first is not available
-Swaps both links and images sources
-aid = identifier for the anchor (a tag)
-imgid = identifier for the image (img tag)
-path = path for the image starting from the root, for example /Dani/10225.jpg
-*/
-function changeUrlToFallback(aid, imgid, path) {
-    try {
-        document.getElementById(imgid).src = "https://drive.vichingo455.freeddns.org/apps/files_sharing/publicpreview/w8Nr4jZN3g6z3pn?file=" + path + "&x=1920&y=1080&a=true";
-    } catch { }
-    try {
-        document.getElementById(aid).href = "https://drive.vichingo455.freeddns.org/apps/files_sharing/publicpreview/w8Nr4jZN3g6z3pn?file=" + path + "&x=1920&y=1080&a=true";
-    } catch { }
-}
-
-function changeUrlToFallbackNoTrue(aid, imgid, path) {
-    try {
-        document.getElementById(imgid).src = "https://drive.vichingo455.freeddns.org/apps/files_sharing/publicpreview/w8Nr4jZN3g6z3pn?file=" + path + "&x=1920&y=1080";
-    } catch { }
-    try {
-        document.getElementById(aid).href = "https://drive.vichingo455.freeddns.org/apps/files_sharing/publicpreview/w8Nr4jZN3g6z3pn?file=" + path + "&x=1920&y=1080";
-    } catch { }
-}
-
 // New fallback system (HA)
 (() => {
     const API_ENDPOINT = "https://ertpl-api.vercel.app/nextcloud";
@@ -36,7 +11,11 @@ function changeUrlToFallbackNoTrue(aid, imgid, path) {
         return photoConfig;
     }
 
-    function buildPreviewUrl(cfg, path, x = 1920, y = 1080, isMenu = false) {
+    function buildPreviewUrl(cfg, path, x = 1920, y = 1080, isMenu = false, isLeo) {
+        if(isLeo){
+            return `${cfg.url}/apps/files_sharing/publicpreview/${cfg.share}` +
+            `?file=${encodeURIComponent(path)}&x=${x}&y=${y}`;
+        }
         return `${cfg.url}/apps/files_sharing/publicpreview/${cfg.share}` +
             `?file=${encodeURIComponent(path)}&x=${x}&y=${y}` + (isMenu ? "" : "&a=true");
     }
@@ -48,12 +27,18 @@ function changeUrlToFallbackNoTrue(aid, imgid, path) {
 
             document.querySelectorAll("img[data-path]").forEach(img => {
                 const path = img.dataset.path;
+                let isLeo = false;
+                const pageUrl = document.location.href;
+                if(pageUrl.includes("/seta_modena/")){
+                    isLeo = true;
+                }
                 const link = img.closest("a");
                 const isMenu = img.classList.contains("bus");
 
-                const url = buildPreviewUrl(cfg, path, 1920, 1080, isMenu);
+                const url = buildPreviewUrl(cfg, path, 1920, 1080, isMenu, false);
+                const imglink = buildPreviewUrl(cfg, path, 1920, 1080, isMenu, isLeo);
 
-                img.src = url;
+                img.src = imglink;
 
                 // aggiorna solo i link che NON finiscono con .html
                 if (link && !link.href.endsWith(".html")) {
