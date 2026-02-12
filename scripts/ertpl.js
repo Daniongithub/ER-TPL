aaaa8a71223de70ec7764960feec91003b1ab41f
The Italian Developer <theitaliandeveloper@proton.me>
2026-02-12 11:20:54 +0100

    Email obfuscation with Base64

--- scripts/ertpl.js
+++ scripts/ertpl.js
@@ -94,17 +94,12 @@ function chisiamoInizializza() {
 // This function below is just a simple obfuscator for the email address, in order to prevent bots invading the inbox.
 function mostraemail() {
     document.getElementById("email").innerHTML = "";
-    const p1 = "info";
-    const p2 = ".";
-    const p3 = "ertpl";
-    const p4 = "@";
-    const p5 = "protonmail";
-    const p6 = ".";
-    const p7 = "com";
+    const baseMail = "aW5mby5lcnRwbEBwcm90b25tYWlsLmNvbQ==";
+    const newMail = atob(baseMail);
     const link = document.getElementById("email").appendChild(document.createElement("a"));
     link.setAttribute("class", "novita")
-    link.innerHTML = p1 + p2 + p3 + p4 + p5 + p6 + p7;
-    link.setAttribute("href", "mailto:" + p1 + p2 + p3 + p4 + p5 + p6 + p7);
+    link.innerHTML = newMail;
+    link.setAttribute("href", "mailto:" + newMail);
 }
 
 // This function below is to display the version of the HA API and the current server
