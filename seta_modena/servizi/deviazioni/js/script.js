575f8c1060af285cdf98535abe9ce58c6492e12e
Leocraft1 <leonardotoschi07@gmail.com>
2026-05-12 17:00:05 +0200

    Aggiornamento deviazioni + nuove foto SETA

--- seta_modena/servizi/deviazioni/js/script.js
+++ seta_modena/servizi/deviazioni/js/script.js
@@ -18,7 +18,35 @@ switch(evento){
 function renderPartita(){
     textContainer.innerHTML=`
         Chiusura al transito di via Monte Kosica, via Berengario e via Fontanelli. <br> 
-        Le linee interessate sono le seguenti: <strong>1, 4, 7, 9, 10, 13</strong>
+        Sono interessate le linee seguenti: <strong>1, 3, 4, 6, 7, 9, 10, 11, 12, 13</strong> <br> <br>
+
+        <strong>Linea 1:</strong> <br>
+        <span style="color: red; font-weight:700;">Percorso sospeso:</span> Largo Garibaldi, Canalgrande, Piazza Roma, Vittorio Emanuele, Stazione FS. <br>
+        <span style="color: lime; font-weight:700;">Percorso temporaneo:</span> Largo Garibaldi, viale Caduti in Guerra, Stazione FS. <br> <br>
+
+        <strong>Linea 4:</strong> <br>
+        <span style="color: red; font-weight:700;">Percorso sospeso:</span> Largo Garibaldi, Canalgrande, Piazza Roma, Vittorio Emanuele, Stazione FS. <br>
+        <span style="color: lime; font-weight:700;">Percorso temporaneo:</span> Largo Garibaldi, viale Caduti in Guerra, Stazione FS. <br> <br>
+
+        <strong>Linea 7:</strong> <br>
+        <span style="color: red; font-weight:700;">Percorso sospeso:</span> Largo Garibaldi, Canalgrande, Piazza Roma, Vittorio Emanuele, Stazione FS. <br>
+        <span style="color: lime; font-weight:700;">Percorso temporaneo:</span> Largo Garibaldi, viale Caduti in Guerra, Stazione FS. <br> <br>
+
+        <strong>Linea 9:</strong> <br>
+        <span style="color: red; font-weight:700;">Percorso sospeso:</span> Largo Garibaldi, Canalgrande, Piazza Roma, Vittorio Emanuele, Stazione FS. <br>
+        <span style="color: lime; font-weight:700;">Percorso temporaneo:</span> Largo Garibaldi, viale Caduti in Guerra, Stazione FS. <br> <br>
+
+        <strong>Linea 10:</strong> <br>
+        <span style="color: red; font-weight:700;">Percorso sospeso:</span> Largo Garibaldi, Canalgrande, Piazza Roma, Vittorio Emanuele, Stazione FS. <br>
+        <span style="color: lime; font-weight:700;">Percorso temporaneo:</span> Largo Garibaldi, viale Caduti in Guerra, Stazione FS. <br> <br>
+
+        <strong>Linea 11:</strong> <br>
+        <span style="color: red; font-weight:700;">Percorso sospeso:</span> Largo Garibaldi, Canalgrande, Piazza Roma, Vittorio Emanuele, Stazione FS. <br>
+        <span style="color: lime; font-weight:700;">Percorso temporaneo:</span> Largo Garibaldi, viale Caduti in Guerra, Stazione FS. <br> <br>
+
+        <strong>Linea 13:</strong> <br>
+        <span style="color: red; font-weight:700;">Percorso sospeso:</span> Largo Garibaldi, Canalgrande, Piazza Roma, Vittorio Emanuele, Stazione FS. <br>
+        <span style="color: lime; font-weight:700;">Percorso temporaneo:</span> Largo Garibaldi, viale Caduti in Guerra, Stazione FS. <br> <br>
     `;
 }
 
