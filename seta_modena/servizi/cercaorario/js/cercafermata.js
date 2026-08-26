53b528db54b7d9473300855fd0061c9317909f5a
Leocraft1 <leonardotoschi07@gmail.com>
2026-08-26 12:26:02 +0200

    Sistemazione home cerca orario SETA + news SETA

--- seta_modena/servizi/cercaorario/js/cercafermata.js
+++ seta_modena/servizi/cercaorario/js/cercafermata.js
@@ -1,15 +1,16 @@
 const API_ENDPOINT = "https://ertpl-api.vichingo455.com/seta";
 
 async function getApiUrl() {
-  const res = await fetch(API_ENDPOINT);
-  const cfg = await res.json();
-  if (cfg.status !== "ok") return null;
-  return cfg.url;
+    const res = await fetch(API_ENDPOINT);
+    const cfg = await res.json();
+    if (cfg.status !== "ok") return null;
+    return cfg.url;
 }
 
 const searchBar = document.getElementById('searchBar');
 const stopCodeBar = document.getElementById('stopCodeBar');
-const resultsContainer = document.getElementById('searchResults');
+const resultsContainer = document.getElementById('results-container');
+const quickContainer = document.getElementById('quick-container');
 const comeLeggere = document.getElementById('comeleggere-p');
 
 var searching = false;
@@ -17,43 +18,40 @@ var oldTerm;
 var allresults = false;
 
 getApiUrl().then(url => {
-fetch(url + "/stoplist")
-    .then(response => {
-        if (!response.ok) throw new Error("Errore nel caricamento dei dati.");
-        return response.json();
-    })
-    .then(data => {
-        allresults = data;
-        if(searching){
-            search(oldTerm);
-        }
-    })
-    .catch(error => console.error('Errore nel caricamento dei dati:', error));})
+    fetch(url + "/stoplist")
+        .then(response => {
+            if (!response.ok) throw new Error("Errore nel caricamento dei dati.");
+            return response.json();
+        })
+        .then(data => {
+            allresults = data;
+            if (searching) {
+                search(oldTerm);
+            }
+        })
+        .catch(error => console.error('Errore nel caricamento dei dati:', error));
+})
 
 
 searchBar.addEventListener('input', () => {
     if (searchBar.value == '') {
-        resultsContainer.innerHTML=`
-            <h3 style="margin-bottom:4px;">Fermate rapide:</h3>
-            <a href="/seta_modena/servizi/cercaorario/altrecorsie.html?location=STAZIONE FS" class="bianco"><div class="search-result"><h3>Autostazione</h3></div></a>
-            <a href="/seta_modena/servizi/cercaorario/altrecorsie.html?location=MODENA AUTOSTAZIONE" class="bianco"><div class="search-result"><h3>Stazione FS</h3></div></a>
-            <a href="/seta_modena/servizi/cercaorario/altrecorsie.html?location=GARIBALDI" class="bianco"><div class="search-result"><h3>Largo Garibaldi</h3></div></a>
-        `;
-        comeLeggere.innerHTML = `
-            <a href="comeleggere.html" style="color: white;">Come leggere il codice fermata.</a>
-        `;
-    }else{
+        resultsContainer.innerHTML = '';
+        quickContainer.style.display = '';
+        comeLeggere.style.display = '';
+    } else {
         const searchTerm = searchBar.value.toLowerCase();
         search(searchTerm);
     }
 });
 
 stopCodeBar.addEventListener('input', () => {
-    var code="MO"+stopCodeBar.value.toUpperCase();
-    comeLeggere.innerHTML='';
+    var code = "MO" + stopCodeBar.value.toUpperCase();
+    comeLeggere.innerHTML = '';
     resultsContainer.innerHTML = '';
 
     const div = document.createElement('div');
+    const a = document.createElement('a');
+    a.href = "fermata.html?code=${code}&name=${code}";
     div.className = 'search-result';
     div.innerHTML = `
         <div>
@@ -61,29 +59,19 @@ stopCodeBar.addEventListener('input', () => {
             <p>Codice fermata: ${code}</p>
         </div>
     `;
-
-    div.addEventListener('click', () => {
-        const url = `fermata.html?code=${code}&name=${code}`;
-        parent.location=url;
-    });
+    a.appendChild(div);
 
     resultsContainer.appendChild(div);
     if (stopCodeBar.value == '') {
-        resultsContainer.innerHTML=`
-            <h3 style="margin-bottom:4px;">Fermate rapide:</h3>
-            <a href="" class="bianco"><div class="search-result"><h3>Autostazione</h3></div></a>
-            <a href="" class="bianco"><div class="search-result"><h3>Stazione FS</h3></div></a>
-            <a href="" class="bianco"><div class="search-result"><h3>Largo Garibaldi</h3></div></a>
-        `;
-        comeLeggere.innerHTML = `
-            <a href="comeleggere.html" style="color: white;">Come leggere il codice fermata.</a>
-        `;
+        resultsContainer.innerHTML = '';
+        quickContainer.style.display = '';
+        comeLeggere.style.display = '';
     }
 });
 
 function renderResults(results) {
-    resultsContainer.innerHTML = '';
-    comeLeggere.innerHTML='';
+    quickContainer.style.display = 'none';
+    comeLeggere.style.display = 'none';
     if (results.length == 0) {
         resultsContainer.innerHTML = '<p>Nessun risultato trovato</p>';
         return;
@@ -92,8 +80,8 @@ function renderResults(results) {
     results.forEach(item => {
         const div = document.createElement('div');
         const a = document.createElement('a');
-        a.setAttribute('class','bianco');
-        a.setAttribute('href',`fermata.html?code=${item.code}&name=${item.name}`);
+        a.setAttribute('class', 'bianco');
+        a.setAttribute('href', `fermata.html?code=${item.code}&name=${item.name}`);
         div.className = 'search-result';
         div.innerHTML = `
             <div>
@@ -107,18 +95,18 @@ function renderResults(results) {
     });
 }
 
-function search(searchTerm){
+function search(searchTerm) {
     //Filters taking first elements as the one starting with the letters in the search term
     searching = true;
     oldTerm = searchTerm;
     const filtered = allresults
-    .filter(item => item.name.toLowerCase().includes(searchTerm))
-    .sort((a, b) => {
-        const aStartsWith = a.name.toLowerCase().startsWith(searchTerm);
-        const bStartsWith = b.name.toLowerCase().startsWith(searchTerm);
-        if (aStartsWith && !bStartsWith) return -1;
-        if (!aStartsWith && bStartsWith) return 1;
-        return 0;
-    });
+        .filter(item => item.name.toLowerCase().includes(searchTerm))
+        .sort((a, b) => {
+            const aStartsWith = a.name.toLowerCase().startsWith(searchTerm);
+            const bStartsWith = b.name.toLowerCase().startsWith(searchTerm);
+            if (aStartsWith && !bStartsWith) return -1;
+            if (!aStartsWith && bStartsWith) return 1;
+            return 0;
+        });
     renderResults(filtered);
 }
